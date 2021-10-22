6bb24091fa5dd4f894f87a342d7e546d109470ba
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2021-10-22 19:12:03 +0200

    remove paypal integration and add standard Landigng Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,19 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { Button, Link } from "@chakra-ui/react";
 import { createUrqlClient } from "../graphql/urqlProvider";
 import { withUrqlClient } from "next-urql";
-import { useMeQuery } from "../graphql/generated/graphql";
-import NextLink from "next/link";
+import { LandingPage } from "../components/LandingPage/LandingPage";
 
 const Home = () => {
-  const [{ data, error, fetching }] = useMeQuery();
-
-  if (data === undefined) {
-    if (error) return <div>Error</div>;
-    return fetching ? <div>Loading</div> : <div>Something went wrong</div>;
-  }
-  if (
-    data.me === undefined ||
-    data.me === null ||
-    data.me.user === null ||
-    data.me.user === undefined
-  ) {
-    return (
-      <div>
-        Something is wrong with your user credentials
-        <NextLink href="/login">
-          <Link>
-            <b>Login again</b>
-          </Link>
-        </NextLink>
-      </div>
-    );
-  }
-
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Weno</title>
         <meta name="description" content="Weno" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Weno</h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <main>
+        <LandingPage />
       </main>
-
-      <Button variant="primaryWeno" borderRadius="6px">
-        Hola
-      </Button>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by Rescens
-        </a>
-      </footer>
+      <footer>Footer</footer>
     </div>
   );
 };
