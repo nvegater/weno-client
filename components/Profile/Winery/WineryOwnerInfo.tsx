ed6e5cc78883954db9b0bf5b596655e22eed4788
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2021-11-26 16:40:22 +0100

    add custom link and connected account message and TODO

--- components/Profile/Winery/WineryOwnerInfo.tsx
+++ components/Profile/Winery/WineryOwnerInfo.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Heading, Flex } from "@chakra-ui/react";
+import { Heading, Flex, Button, Text, Link } from "@chakra-ui/react";
 import {
   CardProperty,
   CardWithUserDetails,
@@ -27,6 +27,7 @@ export const WineryOwnerInfo: FC<WineryOwnerInfoProps> = ({
   winery,
   contextHeader,
 }) => {
+  // TODO modify this query to get an object with all the subscription related information: Subscription, connected account, payment status, count of reservations etc..
   const [
     {
       data: subscriptionStatus,
@@ -74,6 +75,35 @@ export const WineryOwnerInfo: FC<WineryOwnerInfoProps> = ({
         properties={subscriptionProps}
         title="Subscription"
       />
+
+      <Flex justifyContent={[null, null, null, "center"]}>
+        <Heading as="h4" size="md">
+          One more step to start selling your experiences
+        </Heading>
+      </Flex>
+
+      <Text
+        my={4}
+        mx={[0, 0, 0, "5em"]}
+        textAlign={[null, null, null, "center"]}
+      >
+        We want to help you earn money in the most secure way while always being
+        up to date with new regulations. <br /> Thats why we partnered with{" "}
+        <Link
+          fontWeight="bold"
+          href="https://stripe.com/blog/stripe-launches-in-mexico"
+        >
+          Stripe
+        </Link>
+        . Read more about their{" "}
+        <Link href="https://stripe.com/en-mx/privacy">Privacy Policy</Link>.
+      </Text>
+
+      <Flex justifyContent={[null, null, null, "center"]}>
+        <Button variant="secondaryWeno" size="heroWeno">
+          Enable Stripe connected account
+        </Button>
+      </Flex>
     </section>
   );
 };
---
+++ theme/components/link.ts
@@ -0,0 +1,8 @@
+const Link = {
+  baseStyle: {
+    fontWeight: "bold", // Normally, it is "semibold"
+    color: "brand.400",
+  },
+};
+
+export default Link;
