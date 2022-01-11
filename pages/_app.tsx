f24bb776a66a4536c11115b83c6801f685225ce4
sandy <sandiig.1110@gmail.com>
2022-01-11 13:22:15 -0600

    putting a box in index file for language selector

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/provider";
 import theme from "../theme/index";
+import { Box } from "@chakra-ui/react";
 
 import { SSRCookies, SSRKeycloakProvider } from "@react-keycloak/ssr";
 import { KeycloakConfig } from "keycloak-js";
@@ -53,6 +54,7 @@ function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
     <SSRKeycloakProvider {...keycloakConfig}>
       <ChakraProvider theme={theme}>
         <I18nextProvider i18n={i18next}>
+          <Box />
           <Component {...pageProps} />
         </I18nextProvider>
       </ChakraProvider>
