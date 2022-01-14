70731b372804b44137a180d8cb0c0df202eb0dc4
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2022-01-14 13:55:02 +0100

    move create Reservation components to a new component

---
+++ components/Modals/CreateReservation.tsx
@@ -0,0 +1,189 @@
+import React, { FC, useState } from "react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
+import useAuth from "../Authentication/useAuth";
+import {
+  ExperienceInfoFragment,
+  GetCheckoutLinkMutation,
+  GetCheckoutLinkMutationVariables,
+  SlotFragmentFragment,
+  useGetCheckoutLinkMutation,
+} from "../../graphql/generated/graphql";
+import { OperationContext, OperationResult } from "urql";
+import { getToastMessage } from "../utils/chakra-utils";
+
+async function handleBookingLinkRequest(
+  totalPrice: number,
+  experienceInfo: ExperienceInfoFragment,
+  selectedSlot: SlotFragmentFragment,
+  getCheckoutLink: (
+    variables?: GetCheckoutLinkMutationVariables,
+    context?: Partial<OperationContext>
+  ) => Promise<
+    OperationResult<GetCheckoutLinkMutation, GetCheckoutLinkMutationVariables>
+  >,
+  toast,
+  email: string,
+  username?: string
+) {
+  const noOfVisitors = totalPrice / experienceInfo.pricePerPersonInDollars;
+  const webpageBase = window.location.origin;
+  const redirectUri = webpageBase + "/order";
+  const { data, error } = await getCheckoutLink({
+    createCustomerInputs: {
+      email: email,
+      paymentMetadata: username ? { username } : null,
+    },
+    slotId: selectedSlot.id,
+    cancelUrl: webpageBase + "/",
+    successUrl: redirectUri + "/success",
+    noOfVisitors: noOfVisitors,
+  });
+
+  if (error) {
+    toast(getToastMessage("bookingFailed"));
+    console.log(error);
+  }
+
+  if (data?.getCheckoutLink.errors != null) {
+    toast(getToastMessage("bookingNotPossibleServerError"));
+    console.log(data.getCheckoutLink.errors);
+  }
+
+  if (data?.getCheckoutLink.errors == null) {
+    window.location.href = data.getCheckoutLink.link;
+  }
+}
+
+interface CreateReservationProps {
+  experienceInfo: ExperienceInfoFragment;
+  slot: SlotFragmentFragment;
+  totalPrice: number;
+}
+
+export const CreateReservation: FC<CreateReservationProps> = ({
+  experienceInfo,
+  slot,
+  totalPrice,
+}) => {
+  const { authenticated, register, tokenInfo } = useAuth();
+
+  const [submittingBooking, setSubmittingBooking] = useState(false);
+  const [requestGuestEmail, setRequestGuestEmail] = useState(false);
+  const [guestEmail, setGuestEmail] = useState("");
+
+  const [, getCheckoutLink] = useGetCheckoutLinkMutation();
+
+  const toast = useToast();
+  return (
+    <Flex justifyContent={authenticated ? "center" : "space-between"} my={10}>
+      {authenticated && (
+        <Button
+          variant="primaryWeno"
+          size="heroWeno"
+          onClick={async () => {
+            await handleBookingLinkRequest(
+              totalPrice,
+              experienceInfo,
+              slot,
+              getCheckoutLink,
+              toast,
+              tokenInfo.email,
+              tokenInfo.preferred_username
+            );
+          }}
+        >
+          Book
+        </Button>
+      )}
+      {!authenticated && (
+        <>
+          {!requestGuestEmail && (
+            <>
+              <Button
+                variant="primaryWeno"
+                size="heroWeno"
+                onClick={() => {
+                  setRequestGuestEmail(true);
+                }}
+              >
+                Book as a guest
+              </Button>
+
+              <Button
+                size="heroWeno"
+                variant="cta"
+                onClick={() => {
+                  if (!authenticated) {
+                    const webpageBase = window.location.origin;
+                    register({ redirectUri: webpageBase + "/register" });
+                  }
+                }}
+              >
+                Register and book
+              </Button>
+            </>
+          )}
+
+          {requestGuestEmail && (
+            <FormControl
+              display="flex"
+              flexDirection="column"
+              isInvalid={guestEmail === ""}
+              isRequired
+            >
+              <FormLabel htmlFor="guestEmail">
+                We only need your email
+              </FormLabel>
+              <Input
+                type="email"
+                name="guestEmail"
+                onChange={(e) => {
+                  setGuestEmail(e.target.value);
+                }}
+              />
+              <Button
+                size="heroWeno"
+                isLoading={submittingBooking}
+                variant="cta"
+                my={2}
+                onClick={async () => {
+                  const isValidEmail = guestEmail !== "";
+                  if (isValidEmail) {
+                    setSubmittingBooking(true);
+                    await handleBookingLinkRequest(
+                      totalPrice,
+                      experienceInfo,
+                      slot,
+                      getCheckoutLink,
+                      toast,
+                      guestEmail
+                    );
+                    setSubmittingBooking(false);
+                  }
+                }}
+              >
+                Book
+              </Button>
+              {guestEmail !== "" ? (
+                <FormHelperText>
+                  Its where you will receive the booking information
+                </FormHelperText>
+              ) : (
+                <FormErrorMessage>Email is required.</FormErrorMessage>
+              )}
+            </FormControl>
+          )}
+        </>
+      )}
+    </Flex>
+  );
+};
--- components/Modals/Reservation.tsx
+++ components/Modals/Reservation.tsx
@@ -1,26 +1,10 @@
 import React, { FC, useMemo, useState } from "react";
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormHelperText,
-  FormLabel,
-  Heading,
-  Icon,
-  Img,
-  Input,
-  useToast,
-} from "@chakra-ui/react";
+import { Box, Flex, Heading, Icon, Img } from "@chakra-ui/react";
 import {
   ExperienceImageFragmentFragment,
   ExperienceInfoFragment,
   ExperienceWineryInfoFragment,
-  GetCheckoutLinkMutation,
-  GetCheckoutLinkMutationVariables,
   SlotFragmentFragment,
-  useGetCheckoutLinkMutation,
   Valley,
 } from "../../graphql/generated/graphql";
 import { FavoriteExperience } from "../Experiences/FavoriteExperience";
@@ -31,9 +15,7 @@ import { parseISO } from "date-fns";
 import { SlotRadioGroup } from "../Radio/SlotRadioGroup/SlotRadioGroup";
 import { getSlotsFromDate } from "./EditExperienceModal";
 import { InputNumberBox } from "../InputFields/InputNumberBox";
-import { getToastMessage } from "../utils/chakra-utils";
-import { OperationContext, OperationResult } from "urql";
-import useAuth from "../Authentication/useAuth";
+import { CreateReservation } from "./CreateReservation";
 
 interface ExperienceModalLayoutProps {
   experienceWineryInfo: ExperienceWineryInfoFragment;
@@ -46,49 +28,6 @@ interface ExperienceModalLayoutProps {
 const placeHolderImage =
   "https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFjaWFsfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
 
-async function handleBookingLinkRequest(
-  totalPrice: number,
-  experienceInfo: ExperienceInfoFragment,
-  selectedSlot: SlotFragmentFragment,
-  getCheckoutLink: (
-    variables?: GetCheckoutLinkMutationVariables,
-    context?: Partial<OperationContext>
-  ) => Promise<
-    OperationResult<GetCheckoutLinkMutation, GetCheckoutLinkMutationVariables>
-  >,
-  toast,
-  email: string,
-  username?: string
-) {
-  const noOfVisitors = totalPrice / experienceInfo.pricePerPersonInDollars;
-  const webpageBase = window.location.origin;
-  const redirectUri = webpageBase + "/order";
-  const { data, error } = await getCheckoutLink({
-    createCustomerInputs: {
-      email: email,
-      paymentMetadata: username ? { username } : null,
-    },
-    slotId: selectedSlot.id,
-    cancelUrl: webpageBase + "/",
-    successUrl: redirectUri + "/success",
-    noOfVisitors: noOfVisitors,
-  });
-
-  if (error) {
-    toast(getToastMessage("bookingFailed"));
-    console.log(error);
-  }
-
-  if (data?.getCheckoutLink.errors != null) {
-    toast(getToastMessage("bookingNotPossibleServerError"));
-    console.log(data.getCheckoutLink.errors);
-  }
-
-  if (data?.getCheckoutLink.errors == null) {
-    window.location.href = data.getCheckoutLink.link;
-  }
-}
-
 export const Reservation: FC<ExperienceModalLayoutProps> = ({
   images,
   startDateTime,
@@ -96,12 +35,6 @@ export const Reservation: FC<ExperienceModalLayoutProps> = ({
   experienceWineryInfo,
   experienceInfo,
 }) => {
-  const { authenticated, register, tokenInfo } = useAuth();
-
-  const [submittingBooking, setSubmittingBooking] = useState(false);
-  const [requestGuestEmail, setRequestGuestEmail] = useState(false);
-  const [guestEmail, setGuestEmail] = useState("");
-
   const coverImage = images ? images.find((i) => i.coverPage) : null;
 
   const [date, setDate] = useState<string>(startDateTime);
@@ -126,10 +59,6 @@ export const Reservation: FC<ExperienceModalLayoutProps> = ({
   const [selectedSlot, setSelectedSlot] = useState<SlotFragmentFragment>(
     slotsFromDate[0]
   );
-
-  const [, getCheckoutLink] = useGetCheckoutLinkMutation();
-
-  const toast = useToast();
 
   return (
     <Box>
@@ -186,107 +115,11 @@ export const Reservation: FC<ExperienceModalLayoutProps> = ({
         </Heading>
       </Flex>
 
-      <Flex justifyContent={authenticated ? "center" : "space-between"} my={10}>
-        {authenticated && (
-          <Button
-            variant="primaryWeno"
-            size="heroWeno"
-            onClick={async () => {
-              await handleBookingLinkRequest(
-                totalPrice,
-                experienceInfo,
-                selectedSlot,
-                getCheckoutLink,
-                toast,
-                tokenInfo.email,
-                tokenInfo.preferred_username
-              );
-            }}
-          >
-            Book
-          </Button>
-        )}
-        {!authenticated && (
-          <>
-            {!requestGuestEmail && (
-              <>
-                <Button
-                  variant="primaryWeno"
-                  size="heroWeno"
-                  onClick={() => {
-                    setRequestGuestEmail(true);
-                  }}
-                >
-                  Book as a guest
-                </Button>
-
-                <Button
-                  size="heroWeno"
-                  variant="cta"
-                  onClick={() => {
-                    if (!authenticated) {
-                      const webpageBase = window.location.origin;
-                      register({ redirectUri: webpageBase + "/register" });
-                    }
-                  }}
-                >
-                  Register and book
-                </Button>
-              </>
-            )}
-
-            {requestGuestEmail && (
-              <FormControl
-                display="flex"
-                flexDirection="column"
-                isInvalid={guestEmail === ""}
-                isRequired
-              >
-                <FormLabel htmlFor="guestEmail">
-                  We only need your email
-                </FormLabel>
-                <Input
-                  type="email"
-                  name="guestEmail"
-                  onChange={(e) => {
-                    setGuestEmail(e.target.value);
-                  }}
-                />
-                <Button
-                  size="heroWeno"
-                  isLoading={submittingBooking}
-                  variant="cta"
-                  my={2}
-                  onClick={async () => {
-                    const isValidEmail = guestEmail !== "";
-                    if (isValidEmail) {
-                      setSubmittingBooking(true);
-                      await handleBookingLinkRequest(
-                        totalPrice,
-                        experienceInfo,
-                        selectedSlot,
-                        getCheckoutLink,
-                        toast,
-                        guestEmail
-                      );
-                      setSubmittingBooking(false);
-                    }
-                  }}
-                >
-                  Book
-                </Button>
-                {guestEmail !== "" ? (
-                  <FormHelperText>
-                    Its where you will receive the booking information
-                  </FormHelperText>
-                ) : (
-                  <FormErrorMessage>Email is required.</FormErrorMessage>
-                )}
-              </FormControl>
-            )}
-          </>
-        )}
-      </Flex>
+      <CreateReservation
+        experienceInfo={experienceInfo}
+        slot={selectedSlot}
+        totalPrice={totalPrice}
+      />
     </Box>
   );
 };
