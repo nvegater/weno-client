56c8d7e65cd42c68f43ae29be95a68d35eeecf73
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2022-02-10 16:44:33 +0100

    configure upcoming events

--- components/Winery/WineryCalendar.tsx
+++ components/Winery/WineryCalendar.tsx
@@ -2,15 +2,34 @@ import React, { FC } from "react";
 import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { Flex, Heading } from "@chakra-ui/react";
 
 interface WineryCalendarProps {}
 
 export const WineryCalendar: FC<WineryCalendarProps> = ({}) => {
   return (
-    <FullCalendar
-      plugins={[timeGridPlugin, interactionPlugin]}
-      editable
-      selectable
-    />
+    <div>
+      <Flex>
+        <Heading as="h1" size="xl" mb={5}>
+          Upcoming Events
+        </Heading>
+      </Flex>
+      <FullCalendar
+        plugins={[interactionPlugin, timeGridPlugin, dayGridPlugin]}
+        editable
+        selectable
+        headerToolbar={{
+          left: "dayGridMonth,timeGridWeek,timeGridDay",
+          center: "title",
+          right: "today,prev,next",
+        }}
+        initialView="timeGridWeek"
+        nowIndicator={true}
+        height="1000px"
+        timeZone="UTC"
+        scrollTime={"06:00:00"}
+      />
+    </div>
   );
 };
