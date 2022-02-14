879f8bd52ed9c31b38c676a3e4fb70dee916ec6d
Nicolas Vega Terrazas <nicolas.vegaterrazas@bdr.de>
2022-02-14 12:39:03 +0100

    add weno favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,32 @@ const Home = () => {
     <div>
       <Head>
         <title>Weno</title>
-        <meta name="description" content="Weno" />
+        <meta
+          name="description"
+          content="Book events related to the wine industry in Ensenada"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#c35a5a" />
       </Head>
       <main>
         <WenoLayout
