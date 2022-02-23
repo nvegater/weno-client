4b983460de46a2f4c0221ee680992902d8799e2a
sandy <sandiig.1110@gmail.com>
2022-02-22 22:41:05 -0600

    updating suscription card style

--- components/Tiers/PricingCard.tsx
+++ components/Tiers/PricingCard.tsx
@@ -13,7 +13,7 @@ import {
   Text,
   useColorModeValue as mode,
 } from "@chakra-ui/react";
-import { HiArrowNarrowRight, HiCheckCircle } from "react-icons/hi";
+import { HiCheckCircle } from "react-icons/hi";
 import { useTranslation } from "react-i18next";
 
 interface PriceDisplayProps extends FlexProps {
@@ -25,12 +25,12 @@ interface PriceDisplayProps extends FlexProps {
 const PriceDisplay = (props: PriceDisplayProps) => {
   const { currency, price, duration, ...rest } = props;
   return (
-    <Flex w="100%" align="center" fontWeight="extrabold" {...rest}>
-      <Text fontSize="4xl" lineHeight="1" marginEnd="2">
-        {currency}
+    <Flex w="100%" align="center" {...rest}>
+      <Text fontSize="2xl" lineHeight="1" marginEnd="2">
+        ${price}
       </Text>
-      <Text fontSize="6xl" lineHeight="1" letterSpacing="tight">
-        {price}
+      <Text fontSize="2xl" lineHeight="1" letterSpacing="tight">
+        {currency}
       </Text>
       <Text fontSize="2xl" marginStart="1" alignSelf="flex-end">
         {duration}
@@ -83,50 +83,37 @@ export const PricingCard = (props: PricingCardProps) => {
   const [t] = useTranslation("global");
   return (
     <Box
-      bg={mode("white", "gray.700")}
       shadow="md"
       w="full"
       maxW="lg"
       mx="auto"
       rounded="lg"
       overflow="hidden"
+      borderColor={color}
+      borderWidth="thin"
+      color="brand.200"
       {...rest}
     >
-      <Box bg={bg} px="3" py="8" color="white">
-        <Text fontWeight="bold" fontSize="lg">
+      <Box alignItems="center" px="3" py="8">
+        <Text fontWeight="extrabold" fontSize="3xl">
           {name}
         </Text>
-        <PriceDisplay my="2" currency="MXN" price={price} duration={duration} />
-        <Text>{description}</Text>
+        <PriceDisplay my="2" currency="mxn" price={price} duration={duration} />
       </Box>
-      <Box px="8" py="6" borderBottomWidth="1px">
-        <LightMode>
-          <Button
-            onClick={onClick}
-            size="lg"
-            w="full"
-            bg={color}
-            color="white"
-            rightIcon={<HiArrowNarrowRight />}
-          >
-            {t("createAccount")}
-          </Button>
-        </LightMode>
-        <Text
-          mt="2"
-          color={mode("gray.600", "gray.400")}
-          align="center"
-          fontSize="sm"
-        >
-          {t("startOffering")}
-        </Text>
-      </Box>
-      <Box px="8" pt="10" pb="12">
+      <Box px="5" py="5">
         <List stylePosition="outside" spacing="4">
           {features.map((feature, index) => (
             <PricingDetail key={index}>{feature}</PricingDetail>
           ))}
         </List>
+      </Box>
+      <Box px="8" py="6" borderBottomWidth="1px">
+        <LightMode>
+          <Button onClick={onClick} size="md" w="full" bg={color} color="white">
+            {t("choose")}
+            {name}
+          </Button>
+        </LightMode>
       </Box>
     </Box>
   );
